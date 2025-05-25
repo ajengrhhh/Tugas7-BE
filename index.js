42e3f7b1bb7228e616264955b35d9b737d839e5e
Ajeng Anugrah <ajenganugrah@Ajengs-MacBook-Air.local>
2025-05-25 21:33:51 +0700

    update

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ app.set("view engine", "ejs");
 dotenv.config();
 
 app.use(cookieParser());
-app.use(cors({ credentials:true, origin: 'http://localhost:3000' }));
+app.use(cors({ credentials:true, origin:true }));
 app.use(express.json());
 app.get("/", (req, res) => res.render("index"));
 app.use(Loginroute); // <- ini penting
-app.use(NoteRoute);  // <- pastikan huruf kecil sesuai import
+app.use("/notes", NoteRoute);  // <- pastikan huruf kecil sesuai import
 
 app.listen(5001, () => console.log("Server connected"));
